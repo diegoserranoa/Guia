7aa5126441334fb2e706c5a5943425c9f24bae65
Diego Serrano <diegoserranoa@Diegos-MacBook-Pro.local>
2014-10-10 18:42:28 -0500

    first commit

---
+++ Guia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Guia
+//
+//  Created by Diego Serrano on 10/10/14.
+//  Copyright (c) 2014 Diego Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Guia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Guia
+//
+//  Created by Diego Serrano on 10/10/14.
+//  Copyright (c) 2014 Diego Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
